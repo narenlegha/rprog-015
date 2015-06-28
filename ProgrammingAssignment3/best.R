3c87207dca312ea136e78102f85f6b18b8fe59d1
narenlegha <narendralegha.mail@gmail.com>
2015-06-29 03:51:15 +0530

    added error messages and comments

--- ProgrammingAssignment3/best.R
+++ ProgrammingAssignment3/best.R
@@ -1,26 +1,28 @@
 best <- function(state, outcome){
         
+        ## read the csv; colClasses will make reading faster
         data <- read.csv("rprog_data_ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character")
-        
+        ## check validity of state variable
         if(! state %in% data$State){
-                print("invalid state")
+                stop("invlaid state")
         }else if(outcome == "heart attack"){
-                
+                ## separate the relevant columns
                 data_heartattatck <- data[, c("Hospital.Name", "State", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack")]
                 
                 data_heartattatck_state <- subset(data_heartattatck, data_heartattatck$State == state)
-                
+                ## order/sort as per Hospital.Name 
                 data_heartattatck_state_order <- data_heartattatck_state[order(data_heartattatck_state$Hospital.Name),]
                 
                 ## data_heartattatck_state_order[, data_heartattatck_state_order$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack] = as.numeric(data_heartattatck_state_order[, data_heartattatck_state_order$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack])
-                
+                ## this conversion will introduce NAs
                 data_heartattatck_state_order[, 3] = as.numeric(data_heartattatck_state_order[, 3])
-                
+                ## remove NAs inroduced by conversion
                 data_heartattatck_state_order <- data_heartattatck_state_order[complete.cases(data_heartattatck_state_order),]
-                
+                ## return Hosital.Name for which Heart.Attack value is minimum
                 data_heartattatck_state_order$Hospital.Name[data_heartattatck_state_order$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == min(data_heartattatck_state_order$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack)]
                 
         }else if(outcome == "heart failure"){
+        	
                 data_heartfailure <- data[, c("Hospital.Name", "State", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure")]
                 
                 data_heartfailure_state = subset(data_heartfailure, data_heartfailure$State == state)
@@ -34,6 +36,7 @@ best <- function(state, outcome){
                 data_heartfailure_state_order$Hospital.Name[data_heartfailure_state_order$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure == min(data_heartfailure_state_order$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure)]
                 
         }else if(outcome == "pneumonia"){
+
                 data_pneumonia <- data[, c("Hospital.Name", "State", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")]
                 
                 data_pneumonia_state = subset(data_pneumonia, data_pneumonia$State == state)
@@ -47,6 +50,6 @@ best <- function(state, outcome){
                 data_pneumonia_state_order$Hospital.Name[data_pneumonia_state_order$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia == min(data_pneumonia_state_order$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)]
                 
         }else{
-                print("invalid outcome")
+                stop("invalid outcome")
         }
 }
--- ProgrammingAssignment3/rankhospital.R
+++ ProgrammingAssignment3/rankhospital.R
@@ -1,19 +1,24 @@
 rankhospital <- function(state, outcome, num = "best"){
         
+        ## read the csv; colClasses will make reading faster
         data <- read.csv("rprog_data_ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character")
-        if(outcome == "heart attack"){
-                
+        ## check validity of state variable
+        if(!state %in% data$State){
+        	stop("invalid state")
+        }else if(outcome == "heart attack"){
+                ## separate the relevant columns
                 data_outcome <- data[, c("Hospital.Name", "State", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack")]
                 
                 data_outcome_state <- subset(data_outcome, data_outcome$State == state)
+                ## remove rows with NAs
                 data_outcome_state <- data_outcome_state[complete.cases(data_outcome_state),]
-                
+                ## order/sort as per Hospital.Name 
                 data_outcome_state_order <- data_outcome_state[order(data_outcome_state$Hospital.Name),]
-                
+                ## this conversion will introduce NAs
                 data_outcome_state_order[, 3] = as.numeric(data_outcome_state_order[, 3])
-                
+                ## order/sort as per Heart.Attack column values in increasing order; decreasing = FALSE is default option
                 data_outcome_state_order <- data_outcome_state_order[order(data_outcome_state_order$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack),]
-                
+                ## remove NAs inroduced by conversion
                 data_outcome_state_order <- data_outcome_state_order[complete.cases(data_outcome_state_order),]
                 
                 hospital_list <- data_outcome_state_order[ , 1]
@@ -77,5 +82,8 @@ rankhospital <- function(state, outcome, num = "best"){
                 }else{
                         hospital_list[num]
                 }
+        }else{
+        	## If state is valid and outcome is none of the above
+        	stop("invalid outcome")
         }
 }
